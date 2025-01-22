2d70f3114c04b473047db14c4532e1de4df1a0aa
AnastasiLeusenko <anastasileusenko@icloud.com>
2025-01-22 10:07:31 +0000

    Deploying to gh-pages from @ AnastasiLeusenko/goit-js-hw-09@d7a6c8b84a40af6b9daa82f7d93a76a964a5c3d6 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as o}from"./assets/vendor-CgTBfC_f.js";new o(".js-gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"});
+//# sourceMappingURL=1-gallery.js.map
